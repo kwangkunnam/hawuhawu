66f9268d1b768e39b2a9bcc579ff7b01c558e9a3
gnan <gnan@coupang.com>
2016-10-06 16:01:48 +0800

    [kwangkunnam]commit

---
+++ SpringStudy/src/main/java/com/apress/isf/spring/java/model/MyType.java
@@ -0,0 +1,34 @@
+package com.apress.isf.spring.java.model;
+
+/**
+ * Created by gnan on 8/21/16.
+ */
+public class MyType {
+    private String name;
+    private String desc;
+    private String extention;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getExtention() {
+        return extention;
+    }
+
+    public void setExtention(String extention) {
+        this.extention = extention;
+    }
+}
